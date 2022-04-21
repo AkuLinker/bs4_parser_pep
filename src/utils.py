13ff2b45215d491593e0b58cc9ddc039a5a48e0e
AkuLinker <AkuLinker@gmail.com>
2022-04-21 18:50:19 +0300

    added string parameter in find_tag function

--- src/utils.py
+++ src/utils.py
@@ -15,10 +15,10 @@ def get_response(session, url):
             stack_info=True
         )
 
-def find_tag(soup, tag, attrs=None):
-    searched_tag = soup.find(tag, attrs=(attrs or {}))
+def find_tag(soup, tag, attrs=None, string=None):
+    searched_tag = soup.find(tag, attrs=(attrs or {}), string=(string or None))
     if searched_tag is None:
-        error_msg = f'Не найден тег {tag} {attrs}'
+        error_msg = f'Не найден тег {tag} {attrs} {string}'
         logging.error(error_msg, stack_info=True)
         raise ParserFindTagException(error_msg)
     return searched_tag
